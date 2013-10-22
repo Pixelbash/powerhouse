d214a1b2974cde9eaf74c6b9db22d01404977d40
Joe <joe@pixelbash.co.nz>
2013-10-22 14:40:54 +1300

    Fixed LESS import scripts and @p:@v compatibility issue

--- php/header.php
+++ php/header.php
@@ -27,9 +27,19 @@ function js_scripts_method() {
 	wp_enqueue_script('init', get_bloginfo( 'template_url' ) . '/js/init.js',array(
 		'jquery'
 	));
+
+	//enqueue styles
+	wp_enqueue_style( 'main', get_bloginfo( 'template_url' ) . '/less/style.less' );
 } 
 
 add_action('wp_enqueue_scripts', 'js_scripts_method');
 
 //Process .less files
+if (class_exists('WPLessPlugin')){
+	$lessConfig = WPLessPlugin::getInstance()->getConfiguration();
+
+	// compiles in the active theme, in a ‘compiled-css’ subfolder
+	$lessConfig->setUploadDir(get_stylesheet_directory() . '/cache');
+	$lessConfig->setUploadUrl(get_stylesheet_directory_uri() . '/cache');
+}
 add_action('wp_print_styles', array($WPLessPlugin, 'processStylesheets'));
