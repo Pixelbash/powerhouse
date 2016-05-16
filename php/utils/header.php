688071220f219abd782bed198ad32e521fd5a08a
Joe Swann <joe@pixelbash.co.nz>
2016-05-16 13:02:50 +1200

    Pixelbash::Header class

--- php/utils/header.php
+++ php/utils/header.php
@@ -1,20 +1,21 @@
 <?php
+namespace Pixelbash;
 
-//Compile theme options for use in theme header
-function getOptions() {
-	$options = array(
-		'burl' => get_bloginfo('url'),
-		'turl' => get_bloginfo('template_url'),
-		'opts' => get_option('powerhouse')
-	);
+class Header {
+  //Compile theme options for use in theme header
+  static function options() {
+  	$options = array(
+  		'opts' => get_option('powerhouse')
+  	);
 
-	return stripslashes(json_encode($options));
+  	return stripslashes(json_encode($options));
+  }
+
+  //Set up scripts for our theme
+  static function scripts() {
+    add_action('wp_enqueue_scripts', function() {
+    	wp_deregister_script( 'jquery' );
+    	wp_localize_script('init', 'ajax_var', array( 'url' => admin_url('admin-ajax.php'), 'nonce' => wp_create_nonce('ajax-nonce')),1,true);
+    });
+  }
 }
-
-//Set up scripts for our theme
-function setThemeScripts() {
-	wp_deregister_script( 'jquery' );
-	wp_localize_script('init', 'ajax_var', array( 'url' => admin_url('admin-ajax.php'), 'nonce' => wp_create_nonce('ajax-nonce')),1,true);
-} 
-
-add_action('wp_enqueue_scripts', 'setThemeScripts');
