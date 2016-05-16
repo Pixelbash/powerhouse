be9602a9899a3993adca33138916185fa923c8e7
Joe Swann <joe@pixelbash.co.nz>
2016-05-16 13:00:19 +1200

    Post formatting class

--- php/formatters/posts.php
+++ php/formatters/posts.php
@@ -1,40 +1,55 @@
 <?php
 
-function getPosts($opts = array()) {
-
-    //setup loop
+namespace Pixelbash;
+class Post {
+  static function find($opts = array()) {
+      //setup loop
     global $post;
-    $cur_post = $post;
     $posts = array();
-    $args = array( 
-        'order'          => 'DESC', 
-        'orderby'        => 'menu_order', 
-        'post_type'      => 'testimonial', 
-        'post_status'    => 'publish',
-        'posts_per_page' => 12
+    $defaults = array( 
+      'order'          => 'DESC', 
+      'orderby'        => 'menu_order date', 
+      'post_type'      => 'post', 
+      'post_status'    => 'publish',
+      'posts_per_page' => 12
     );
 
-    wp_reset_query();
-    $loop = new WP_Query( $args );
+    $args = array_merge($defaults,$opts);
+
+    wp_reset_query(); query_posts($args); //Set up default query for pagination etc
+   
+    $loop = new \WP_Query( $args );
 
     while ( $loop->have_posts() ) {
-        $loop->the_post();
-        $posts[] = getPost($cur_post);
+      $loop->the_post();
+      $posts[] = self::get($post->ID);
     } 
 
     return $posts;
-}
+  }
 
-function getPost($cur_post = false) {
-    $post = new TimberPost();
+  static function get($id = false) {
+    $post = new \TimberPost($id);
+
+    $thumb_id = get_post_thumbnail_id($post->ID);
+    $image = Image::resize([
+      'id'     => $thumb_id,
+      'width'  => 400,
+      'height' => 300,
+      'resize' => 'adaptive_resize'
+    ]);
 
     $post_data = array(
-        'id'          => $post->ID,
-        'title'       => $post->post_title,
-        'href'        => get_permalink(),
-        'testimonial' => $post->JS_testimonial,
-        'attribution' => $post->JS_attribution
-    );
+     'id'          => $post->ID,
+     'title'       => $post->post_title,
+     'author'      => get_the_author_meta('display_name',$post->post_author),
+     'excerpt'     => get_the_excerpt(),
+     'content'     => apply_filters('the_content', $post->post_content),
+     'href'        => get_permalink(),
+     'image'       => $image
+   );
 
     return $post_data;
+  }
+
 }
