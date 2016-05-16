a69d5589f17cd61449281ecc454879f4c1a8a80a
Joe Swann <joe@pixelbash.co.nz>
2016-05-16 13:14:33 +1200

    Posts formatting with pagination

--- archive.php
+++ archive.php
@@ -3,7 +3,10 @@
 $context = Timber::get_context();
 $post    = new TimberPost();
 
-$context['title']   = $post->post_title;
-$context['posts']   = getPosts();
+$context['posts'] = Pixelbash\Post::find();
+$context['pagination'] = Timber::get_pagination();
 
-Timber::render('archive.twig', $context);
+// Important, pagination needs to come after query_posts($args);
+// See Pixelbash\Post::find(); for details
+
+Timber::render('p-posts.twig', $context);
