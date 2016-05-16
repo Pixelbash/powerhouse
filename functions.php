66f9afef4cb143fb75b31d80e6ee65634406a31f
Joe Swann <joe@pixelbash.co.nz>
2016-05-16 13:10:11 +1200

    Updated functions.php

--- functions.php
+++ functions.php
@@ -24,20 +24,17 @@ require_once TEMPLATEPATH . '/vendor/abeautifulsite/simpleimage/src/abeautifulsi
 //utilities
 require_once TEMPLATEPATH . '/php/utils/pluralizer.php';
 require_once TEMPLATEPATH . '/php/utils/post_types.php';
-require_once TEMPLATEPATH . '/php/utils/image_resizer.php';
 require_once TEMPLATEPATH . '/php/utils/image_functions.php';
 require_once TEMPLATEPATH . '/php/utils/header.php';
 require_once TEMPLATEPATH . '/php/utils/attachment.php';
 require_once TEMPLATEPATH . '/php/utils/form.php';
-
-//forms
-//require_once TEMPLATEPATH . '/php/forms/register.php';
 
 //optional
 //require_once TEMPLATEPATH . '/php/shortcodes/block.php';
 
 //post types
 require_once TEMPLATEPATH . '/php/formatters/posts.php';
+require_once TEMPLATEPATH . '/php/formatters/pages.php';
 
 //site specific
 require_once TEMPLATEPATH . '/php/post_types.php';
@@ -45,3 +42,4 @@ require_once TEMPLATEPATH . '/php/metaboxes.php';
 require_once TEMPLATEPATH . '/php/wordpress.php';
 require_once TEMPLATEPATH . '/php/timber.php';
 require_once TEMPLATEPATH . '/php/menu.php';
+require_once TEMPLATEPATH . '/php/forms.php';
