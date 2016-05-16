ac9485e7d5ac1bd6045982511b0d79f5e7f75f68
Joe Swann <joe@pixelbash.co.nz>
2016-05-16 13:07:21 +1200

    Example timber context setup

--- php/timber.php
+++ php/timber.php
@@ -1,12 +1,18 @@
 <?php
 
+namespace Pixelbash;
+
 // Timber context
-add_filter('timber_context', 'jsAddToContext');
-function jsAddToContext($data){
-  $data['options']   = get_option('js_options');
+add_filter('timber_context', function($data){
+  $options = get_option('powerhouse');
+  $data['options']   = $options;
   $data['year']      = date('Y');
   $data['is_home']   = is_home();
-  $data['main_menu'] = getMainMenu();
+  $data['main_menu'] = Menu::get('header');
+  $data['mobile_menu'] = Menu::get('mobile');
+  $data['footer_menu'] = Menu::get('footer');
+
+  $data['template_uri'] = \get_template_directory_uri();
   
   return $data;
-}
+});
