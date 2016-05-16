9fd3d93d1e311d258d86d6413adb6921652f0814
Joe Swann <joe@pixelbash.co.nz>
2016-05-16 13:01:56 +1200

    Pixelbash::Attachment class

--- php/utils/attachment.php
+++ php/utils/attachment.php
@@ -1,15 +1,18 @@
 <?php
+namespace Pixelbash;
 
-//function that gets formatted attachment meta
-function jsGetAttachment( $attachment_id ) {
+class Attachment {
+  //function that gets formatted attachment meta
+  function get( $attachment_id ) {
 
-	$attachment = get_post( $attachment_id );
-	return array(
-		'alt'         => get_post_meta( $attachment->ID, '_wp_attachment_image_alt', true ),
-		'caption'     => $attachment->post_excerpt,
-		'description' => $attachment->post_content,
-		'href'        => get_permalink( $attachment->ID ),
-		'src'         => $attachment->guid,
-		'title'       => $attachment->post_title
-	);
+    $attachment = get_post( $attachment_id );
+    return array(
+      'alt'         => get_post_meta( $attachment->ID, '_wp_attachment_image_alt', true ),
+      'caption'     => $attachment->post_excerpt,
+      'description' => $attachment->post_content,
+      'href'        => get_permalink( $attachment->ID ),
+      'src'         => $attachment->guid,
+      'title'       => $attachment->post_title
+    );
+  }  
 }
