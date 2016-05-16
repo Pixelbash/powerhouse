09fa470709ceedeb3e707e3fd8c2271b3e1cbf4d
Joe Swann <joe@pixelbash.co.nz>
2016-05-16 13:02:19 +1200

    Pixelbash::Form class

--- php/utils/form.php
+++ php/utils/form.php
@@ -1,6 +1,7 @@
 <?php
+namespace Pixelbash;
 
-class jsForm {
+class Form {
   private $submitted  = false;
   private $success    = false;
   private $has_errors = false;
@@ -9,27 +10,30 @@ class jsForm {
   private $post       = array(); //post variables
   private $holder     = array(); //values for the template form
   private $errors     = array();
-  private $form       = array(); //form settings and results
+  private $inputs       = array(); //form settings and results
   private $message    = array(); //email settings
   public  $results    = array(); //template variables
   
   function __construct($settings) {
     //get our settings
     $this->post    = $_POST;
-    $this->form    = $settings['form'];
+    $this->inputs  = $settings['inputs'];
     $this->message = $settings['message'];
 
+    //Add submit to input array
+    $this->inputs['submit'] = array();
+
     //set up our form
-    foreach ($this->form as $key => $item){
+    foreach ($this->inputs as $key => $item){
       $var = (isset($this->post[$key])) ? $this->post[$key] : false;
 
-      $this->form[$key]['var'] = $var;
+      $this->inputs[$key]['var'] = $var;
       $this->holder[$key]      = $var;
       $this->errors[$key]      = false;
     }
 
     //run our class if we are submitting the form
-    if($this->form['submit']['var']){
+    if(isset($this->inputs['submit']['var']) && $this->inputs['submit']['var']) {
       $this->submitted = true;
       $this->init();
     }
@@ -42,6 +46,7 @@ class jsForm {
     //control structure
     if(!$this->has_errors) {
       $this->send_email();
+      //Register with newsletter if allowed
     }
 
     //set our template variables
@@ -57,24 +62,27 @@ class jsForm {
 
   function validate() {
     //foreach input
-    foreach ($this->form as $key => $item){
-
-
+    foreach ($this->inputs as $key => $item){
       //if empty
-      if(!$item['var'] && $item['validate'] == true) {
+      if($key !== 'submit' && $item['validate'] == true) {
+        if(!$item['var']) {
 
-        $this->errors[$key] = "<div class='error'>{$item['error']}</div>";
-        $this->has_errors   = true;
+          $this->errors[$key] = "<div class='error'>{$item['error']}</div>";
+          $this->has_errors   = true;
 
-      //if not empty but still wrong
-      } elseif( $item['var'] && $item['validate'] == true ) {
+        //if not empty but still wrong
+        } elseif( $item['var']) {
 
-        //validate email addresses
-        if($key == 'r_email'){
-          if(!preg_match("^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$^", $item['var'])) {
-            $this->errors[$key] = "<div class='error'>{$item['email']}</div>";
-            $this->has_errors   = true;
-          }
+          //validate email addresses
+          if(stristr($key,'email')) {
+            if(!preg_match("^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$^", $item['var'])) {
+              $this->errors[$key] = "<div class='error'>{$item['email']}</div>";
+              $this->has_errors   = true;
+            }
+          } 
+          //elseif($item['function']) {
+          //   $item['function']();
+          // }
         }
       }
     }
@@ -84,7 +92,7 @@ class jsForm {
     $email_subject = $this->message['subject'];
     $email_to      = $this->message['to'];
     $email_headers = $this->message['headers'];
-    $email_name    = $this->form['r_fname']['var'];
+    $email_name    = $this->message['name'];
     $email_message = vsprintf($this->message['template'], $this->holder);
 
     if(wp_mail( $email_to, $email_subject, $email_message, $email_headers )) {
@@ -98,8 +106,8 @@ class jsForm {
 
   function holder() {
     if($this->success) {
-      foreach ($this->form as $key => $item){
-        $this->holder[$key]   = '';
+      foreach ($this->inputs as $key => $item) {
+        //$this->holder[$key] = '';
       }
     }
   }
